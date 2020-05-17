4dd734032a12d793502f13e9512570ab72e4eb8f
thomas-galligan <tom.galligan@beamery.com>
2020-05-17 19:24:48 +0100

    add skeleton

---
+++ build/precache-manifest.650be3abf8f99afcd01505777b72c09d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a28d4d33d1457253857a09fb303f8fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa61e66589813380a772",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd25bf603745ea560b61",
+    "url": "/static/js/2.c71f1797.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c71f1797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa61e66589813380a772",
+    "url": "/static/js/main.0dd71f13.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
