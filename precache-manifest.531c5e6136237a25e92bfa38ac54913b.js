e0eaf2ec4ffa8f298250e93cb67146411c7743ce
thomas-galligan <tom.galligan@beamery.com>
2020-05-17 19:45:57 +0100

    Updates

--- precache-manifest.531c5e6136237a25e92bfa38ac54913b.js
+++ precache-manifest.531c5e6136237a25e92bfa38ac54913b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d070e065e3a85c3b72f67f1ae9b8804d",
+    "revision": "c22714cf9e4e10aeea3cb1ad50e0b0e0",
     "url": "/index.html"
   },
   {
-    "revision": "57719e1bc2b49e9f1e1e",
+    "revision": "b764a598ddb250f955ca",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "bd25bf603745ea560b61",
-    "url": "/static/js/2.c71f1797.chunk.js"
+    "revision": "704581065d5f340ae0dd",
+    "url": "/static/js/2.6bd14992.chunk.js"
   },
   {
-    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.c71f1797.chunk.js.LICENSE.txt"
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6bd14992.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "57719e1bc2b49e9f1e1e",
-    "url": "/static/js/main.a491c0b7.chunk.js"
+    "revision": "b764a598ddb250f955ca",
+    "url": "/static/js/main.d4e13745.chunk.js"
   },
   {
     "revision": "447edeee62eb2871bcd0",
