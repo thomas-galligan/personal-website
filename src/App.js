66a507b8a41aeb7be9c9eb9fbc32fa0f2aae4de4
thomas-galligan <tom.galligan@beamery.com>
2020-05-17 19:44:55 +0100

    add blog page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core'
 
 function App() {
+  const [page, setPage] = useState("home")
+
+  switch (page) {
+    case "home":
+      return home({ setPage })
+    case "blog":
+      return blog({ setPage })
+  }
+}
+
+const home = ({ setPage }) => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Tom Galligan</h1>
         <p>
           Hello! This will be my website/blog. Check back soon for updates.
         </p>
+        <Button variant="contained" onClick={() => setPage("blog")}>Blog</Button>
       </header>
     </div>
+
   );
 }
 
+const blog = ({ setPage }) => {
+  return (
+    <div className="Blog">
+      <header>
+        <h1>This is going to be my blog.</h1>
+      </header>
+    </div>
+  )
+}
 export default App;
